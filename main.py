8aa2012db28e957ca8f4c2c28d5a0375ba7563db
Wizard-Wade <wade.vollink@gmail.com>
2024-12-09 08:53:20 -0700

    draw screen as black backgound continuously. listen for event to exit game

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import pygame
+from constants import *
+
+screen = pygame.Surface((0,0))
 
 def main():
     print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gameloop()
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+def gameloop():
+    while(True):
+        if window_close(): return
+        screen.fill(pygame.Color(0,0,0))
+        
+        pygame.display.flip()
+        
+def window_close():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return True
 
 if __name__ == "__main__":
     main()
