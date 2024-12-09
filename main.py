2dc35f35b732410e01c7da85f20b55bdb047b37a
Wizard-Wade <wade.vollink@gmail.com>
2024-12-09 09:17:16 -0700

    add game clock to set max fps

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ class asteroids_game():
     def __init__(self):
         print("Starting asteroids!")
         pygame.init()
+        self.__fps = game_clock()
+        
         self.__screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.gameloop()
     
@@ -14,11 +16,20 @@ class asteroids_game():
             self.__screen.fill(pygame.Color(0,0,0))
             
             pygame.display.flip()
+            self.__fps.addframe()
             
     def window_close(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return True
+
+class game_clock():
+    def __init__(self):
+        self.__clock = pygame.time.Clock()
+        self.__dt = 0
+    
+    def addframe(self):
+        self.__dt = self.__clock.tick(60)/1000
 
 def main():
     asteroids_game()
